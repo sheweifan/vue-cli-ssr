35cb845f091a83887917d09aa7db31c7a32d62dd
weifan she <923986234@qq.com>
2020-09-25 10:23:08 +0800

    remove useless dependencies

--- src/views/About.vue
+++ src/views/About.vue
@@ -6,14 +6,15 @@
 
 <script>
 export default {
-
+  created() {
+    console.log(`process.env.VUE_ENV`, process.env.VUE_ENV)
+    console.log(`process.env.TARGET_NODE`, process.env)
+  }
 }
 </script>
 
 <style>
-
-.about{
-  background: red
+.about {
+  background: red;
 }
-
 </style>
